203f9f1b6543278030de65339163c2f51792f2f4
Samuel Lopez Mejia <samuelmejialorz@gmail.com>
2025-11-28 20:29:25 -0600

    Se agrego el archivo TipoDeDatos

---
+++ basic/TiposDeDatos.java
@@ -0,0 +1,21 @@
+package basic;
+
+public class TiposDeDatos {
+    public static void main(String[] args) {
+        /*
+        Tipo de dato entero.
+        Valor mínimo: -2,147,483,648
+        Valor máximo: 2,147,483,647 
+        - >
+         */
+        int numero = 1; 
+        /*
+        Tipo de dato flotante
+        Valor: ±3.4 x 10³⁸
+        6-7 dígitos
+        - >
+         */
+        float decimalflotante = 1.0993434f;
+        
+    }
+}
