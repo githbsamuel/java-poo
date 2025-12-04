e84f75c0f686c6d997386cdef3d6c312b87bf5f5
Samuel Lopez Mejia <samuelmejialorz@gmail.com>
2025-12-03 22:13:34 -0600

    Se añadieron archivos nuevos

---
+++ basic/EstructuraIf.java
@@ -0,0 +1,32 @@
+package basic;
+
+public class EstructuraIf {
+    public static void main(String[] args) {
+        //Estructura if, sirve para tomar decisiones.
+
+        /*
+        Sintaxis de if
+
+        if(condicion){
+        //bloque de codigo
+        }
+        else{
+        //bloque de codigo    
+        }
+        */
+        //ejemplo
+        if(1>2){
+            System.out.println("Verdad");
+        }
+        else{
+            System.out.println("Falso");
+        }
+
+        /*
+        Salida : Falso
+        ¿Porque?
+        1 no es mayor que 2
+        */
+    }
+    
+}
--- basic/TiposDeDatos.java
+++ basic/TiposDeDatos.java
@@ -2,20 +2,54 @@ package basic;
 
 public class TiposDeDatos {
     public static void main(String[] args) {
+        // Datos primitivos
         /*
-        Tipo de dato entero.
-        Valor mínimo: -2,147,483,648
-        Valor máximo: 2,147,483,647 
-        - >
+         * ¿Qué son?
+         * tipos de datos básicos que representan valores simples y se almacenan
+         * directamente en la memoria
+         * 
          */
-        int numero = 1; 
+        byte numeroByte =  127;
+        System.out.println(numeroByte);
         /*
-        Tipo de dato flotante
-        Valor: ±3.4 x 10³⁸
-        6-7 dígitos
-        - >
          */
-        float decimalflotante = 1.0993434f;
-        
+        short numeroShort = 32767;
+        System.out.println(numeroShort);
+        /*
+         */
+        long numeroLong = 922337203;
+        System.out.println(numeroLong);
+        /*
+         * Tipo de dato entero.
+         * Valor mínimo: -2,147,483,648
+         * Valor máximo: 2,147,483,647
+         * - >
+         */
+        int numeroInt = 1;
+        System.out.println(numeroInt);
+        /*
+         * Tipo de dato flotante
+         * Valor: ±3.4 x 10³⁸
+         * 6-7 dígitos
+         * - >
+         */
+        float numerofloat = 1.0993434f;
+        System.out.println(numerofloat);
+        /*
+         */
+        double numeroDou = 1.000929392908909009090909090909097888978789789787878978973;
+        System.out.println(numeroDou);
+       
+        //No primitivos
+         /*
+         * ¿Qué son?
+         * son tipos más complejos como objetos, arrays y cadenas,
+         * que tienen funciones o métodos asociados.
+         */
+
+     
+        /*
+         */
+
     }
 }
